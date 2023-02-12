3a6169bca8ac5fddc9e58adf491f0cc9003b7e99
Aria Gomes <aria.gomes00@gmail.com>
2023-02-11 20:48:16 -0500

    Working pull with logging

--- commands/pull.js
+++ commands/pull.js
@@ -2,30 +2,49 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const Docker = require("dockerode");
 var docker = new Docker({ socketPath: "/var/run/docker.sock" });
 
-let result = null
-
-//Lists all started containers
-docker.pull("debian", (err, containers) => {
-	if (err) {
-        result = {...err};
-	} else {
-        result = {...containers};
-	}
-  });
+let progressMsg
 
 module.exports = {
-    data: new SlashCommandBuilder()
-      .setName("pull")
-      .setDescription(
-        "Pulls a container and replies with result"
-      ),
-      async execute(interaction) {
- 
-        console.log(result)
-  
-        interaction.reply(
-          JSON.stringify(result)
-        );
-      }
-  };
-  
+  data: new SlashCommandBuilder()
+    .setName("pull")
+    .setDescription("Pulls a container and replies with result")
+    .addStringOption((image) =>
+      image
+        .setName("image")
+        .setDescription("image:tag you would like to pull")
+        .setRequired(true)
+    ),
+    async execute(interaction) {
+        const imageName = interaction.options.getString('image');
+        progressMsg = ''
+        await interaction.reply(`Pulling ${imageName} Docker image...`);
+    
+        docker.pull(imageName, (err, stream) => {
+          if (err) return interaction.editReply(`Error Pulling ${imageName}\n\n${progressMsg}`);
+    
+          let buffer = '';
+          stream.on('data', (chunk) => {
+            buffer += chunk.toString();
+            const progressMsg = buffer
+              .split('\n')
+              .filter(Boolean)
+              .map((line) => '> ' + line)
+              .join('\n');
+    
+            interaction.editReply(`Pulling ${imageName} Docker image...\n\n${progressMsg}`);
+          });
+    
+          stream.on('end', () => {
+           progressMsg = buffer
+              .split('\n')
+              .filter(Boolean)
+              .map((line) => '> ' + line)
+              .join('\n');
+    
+            interaction.editReply(`Successfully pulled ${imageName} Docker image!\n\n${progressMsg}`);
+          });
+        });
+      
+    
+  },
+};
